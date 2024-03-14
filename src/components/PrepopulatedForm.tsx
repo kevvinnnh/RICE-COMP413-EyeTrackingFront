e44e10d6e757a57d648d47d1b1b23de7e1f88e44
Sophia Rebello <sir5@rice.edu>
2024-03-14 11:08:59 -0500

    Added images to default survey

--- src/components/PrepopulatedForm.tsx
+++ src/components/PrepopulatedForm.tsx
@@ -1,6 +1,7 @@
 // PrepopulatedForm.tsx
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { HOSTNAME } from '../HostName.tsx'
 
 const PrepopulatedForm = () => {
     // Assume there's a state to manage form responses, etc.
@@ -13,7 +14,7 @@ const PrepopulatedForm = () => {
         // You would handle form submission here
         // For example, POST to backend
         try {
-            const response = await fetch('http://localhost:5001/api/responses', {
+            const response = await fetch(`${HOSTNAME}/api/responses`, {
                 
                 method: 'POST',
                 headers: {
@@ -53,15 +54,18 @@ const PrepopulatedForm = () => {
                 <label className="text-sm font-medium text-gray-700">
                     Please select your role:
                     <select name="role" className="p-2.5 text-sm border-[0px] bg-gray-100 rounded-md w-full">
+                    <option value=""></option> 
                     <option value="physician">Physician</option>
                     <option value="staff">Staff</option>
                     <option value="admin">Admin</option>
                     </select>
                 </label>
+            </div>
         {/* Experience Level question */}
         <div className="bg-white border-[1px] rounded-lg p-2 flex flex-col w-full">
             <label className="text-sm font-medium text-gray-700">Experience Level:</label>
             <select name="experienceLevel" className="p-2.5 text-sm border-[0px] bg-gray-100 rounded-md w-full">
+                <option value=""></option> 
                 <option value="beginner">Beginner</option>
                 <option value="intermediate">Intermediate</option>
                 <option value="advanced">Advanced</option>
@@ -83,6 +87,7 @@ const PrepopulatedForm = () => {
             <label className="text-sm font-medium text-gray-700">
                 What is your gender?
                 <select name="gender" className="form-select mt-1 block w-full">
+                    <option value=""></option> 
                     <option value="male">Male</option>
                     <option value="female">Female</option>
                     <option value="other">Other</option>
@@ -109,13 +114,14 @@ const PrepopulatedForm = () => {
         </div>
 
         {/* Skin Lesion Diagnosis Questions*/}
+        {/* Question 1 */}
         <div className="bg-white border-[1px] rounded-lg p-2 flex flex-col w-full mb-4">
         <label className="text-sm font-medium text-gray-700">
-            What is the diagnosis for this skin lesion on the individual’s <em>back</em>?
+            1. What is the diagnosis for this skin lesion on the individual’s <em>back</em>?
         </label>
         <img
             // Replace with your path to image file 
-            src="C:\Users\sophi\comp413\RICE-COMP413-EyeTrackingFront\src\components\dataverse_files.zip"
+            src="\default_survey_images\ISIC_0034539_1.jpg"
             className="my-4"
         />
         <div className="flex flex-col">
@@ -136,8 +142,65 @@ const PrepopulatedForm = () => {
             <span className="ml-2">Melanocytic nevi</span>
             </label>
         </div>
+
+         {/* Question 2 */}
+         <div className="bg-white border-[1px] rounded-lg p-2 flex flex-col w-full mb-4">
+        <label className="text-sm font-medium text-gray-700">
+            2. What is the diagnosis for this skin lesion on the individual’s <em>abdomen</em>?
+        </label>
+        <img
+            // Replace with your path to image file 
+            src="\default_survey_images\ISIC_0034525_2.jpg"
+            className="my-4"
+        />
+        <div className="flex flex-col">
+            <label className="inline-flex items-center">
+            <input type="radio" name="diagnosis" value="melanocyticNevi" className="form-radio" />
+            <span className="ml-2">Melanocytic nevi</span>
+            </label>
+            <label className="inline-flex items-center">
+            <input type="radio" name="diagnosis" value="benignKeratosis" className="form-radio" />
+            <span className="ml-2">Benign keratosis-like lesions</span>
+            </label>
+            <label className="inline-flex items-center">
+            <input type="radio" name="diagnosis" value="vascularLesions" className="form-radio" />
+            <span className="ml-2">Vascular lesions</span>
+            </label>
+            <label className="inline-flex items-center">
+            <input type="radio" name="diagnosis" value="basalCellCarcinoma" className="form-radio" />
+            <span className="ml-2">Basal cell carcinoma</span>
+            </label>
         </div>
 
+        {/* Question 3 */}
+        <div className="bg-white border-[1px] rounded-lg p-2 flex flex-col w-full mb-4">
+        <label className="text-sm font-medium text-gray-700">
+            3. What is the diagnosis for this skin lesion on the individual’s <em>back</em>?
+        </label>
+        <img
+            // Replace with your path to image file 
+            src="\default_survey_images\ISIC_0034526_3.jpg"
+            className="my-4"
+        />
+        <div className="flex flex-col">
+            <label className="inline-flex items-center">
+            <input type="radio" name="diagnosis" value="benignKeratosis" className="form-radio" />
+            <span className="ml-2">Benign keratosis-like lesions</span>
+            </label>
+            <label className="inline-flex items-center">
+            <input type="radio" name="diagnosis" value="basalCellCarcinoma" className="form-radio" />
+            <span className="ml-2">Basal cell carcinoma</span>
+            </label>
+            <label className="inline-flex items-center">
+            <input type="radio" name="diagnosis" value="vascularLesions" className="form-radio" />
+            <span className="ml-2">Vascular lesions</span>
+            </label>
+            <label className="inline-flex items-center">
+            <input type="radio" name="diagnosis" value="dermatofibroma" className="form-radio" />
+            <span className="ml-2">Dermatofibroma</span>
+            </label>
+        </div>
+        </div></div>
                 <button type="submit" className="mt-4 bg-gradient-to-tr from-blue-600 to-blue-400 text-white font-bold text-lg py-2 px-4 rounded transition duration-300 ease-in-out transform hover:translate-x-1 hover:shadow-lg">
                     Submit Responses
                 </button>
